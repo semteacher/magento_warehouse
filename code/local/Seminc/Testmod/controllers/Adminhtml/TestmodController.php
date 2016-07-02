ec4467ba47376f4bb1c8fea315c38b0441e34aa1
semteacher <semteacher@gmail.com>
2016-07-02 13:48:21 +0300

    module backend attempt

---
+++ code/local/Seminc/Testmod/controllers/Adminhtml/TestmodController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SemenetsA
+ * Date: 01.07.2016
+ * Time: 17:13
+ */
+class Seminc_Testmod_Adminhtml_TestmodController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+        return $this;
+    }
+}
