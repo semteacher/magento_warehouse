0b7b87b12aa925e75b9e6f8ab0247f5b6b45b296
semteacher <semteacher@gmail.com>
2016-07-10 21:47:35 +0300

    experiments: 1) /testmod page with products list 2) customer registration block form added to the /testmod page 3) add block to the product view page

--- code/local/Seminc/Testmod/Block/Testmod.php
+++ code/local/Seminc/Testmod/Block/Testmod.php
@@ -5,4 +5,22 @@ class Seminc_Testmod_Block_Testmod extends Mage_Core_Block_Template
     {
         return "Hello tuts+ world - we are in the Block mode now";
     }
+    
+    public function testproductfunction()
+    {
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('sku')
+            ->addAttributeToSelect('qty')
+            ->addAttributeToSelect('store');
+        $collection->setOrder('name', 'asc');
+        $collection->load();
+
+        return $collection;
+    }
+
+    public function prodinfo2blocfunction()
+    {
+        return "Hello world - we are in the Product page now";
+    }
 }
