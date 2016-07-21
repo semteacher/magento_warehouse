c85efd5578cc87894453902ad9c22f9b0e40e44d
semteacher <semteacher@gmail.com>
2016-07-21 09:51:09 +0300

    model classes added

---
+++ code/local/Seminc/Warehousecsv/Model/Warehouse.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SemenetsA
+ * Date: 21.07.2016
+ * Time: 9:48
+ */
+class Seminc_Warehousecsv_Model_Warehouse extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('warehousecsv/warehouse');
+    }
+}
